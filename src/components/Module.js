72e5678c84b7310e4c28087cfdebde50519b2f0a
Cpraveen2002 <praveenkumar.ch20@iiits.in>
2026-02-15 12:24:00 +0530

    refactor: update layout structure in Module component for improved responsiveness

--- src/components/Module.js
+++ src/components/Module.js
@@ -15,81 +15,83 @@ const Module = () => {
             <p className='text-left text-gray-500 font-medium text-sm lg:text-md'>
                 Designed for a global HR data-driven operating model.
             </p>
-            <div className='w-full flex  gap-2 justify-start items-center shadow-md px-2 py-2 rounded-xl'>
-                <BiCategoryAlt className='w-6 h-6 lg:w-8 lg:h-8 bg-blue-100 px-1 rounded-xl text-gray-600' />
-                <div className='flex flex-col justify-start items-start'>
-                    <p className='text-sm lg:text-md font-semibold text-left'>
-                        Executive Overview
-                    </p>
-                    <p className='text-left text-gray-500 text-sm lg:text-md font-semibold text-left'>
-                        KPIs + drill-down
-                    </p>
+            <div className='grid grid-cols-4 gap-2 md:grid-cols-4 lg:grid-cols-1'>
+                <div className='w-full flex  gap-2 justify-start items-center shadow-md px-2 py-2 rounded-xl'>
+                    <BiCategoryAlt className='w-6 h-6 lg:w-8 lg:h-8 bg-blue-100 px-1 rounded-xl text-gray-600' />
+                    <div className='flex flex-col justify-start items-start'>
+                        <p className='text-sm lg:text-md font-semibold text-left'>
+                            Executive Overview
+                        </p>
+                        <p className='text-left text-gray-500 text-sm lg:text-md font-semibold text-left'>
+                            KPIs + drill-down
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div className='w-full flex  gap-2 justify-start items-center shadow-md px-2 py-2 rounded-xl'>
-                <LuGitCompare className='w-6 h-6 lg:w-8 lg:h-8 bg-blue-100 px-1 rounded-xl text-gray-600' />
-                <div className='flex flex-col justify-start items-start'>
-                    <p className='text-sm lg:text-md font-semibold lg:font-semibold text-left'>
-                        Workforce Planning
-                    </p>
-                    <p className='text-left text-gray-500 font-semibold text-sm lg:text-md'>
-                        Scenarios + gaps
-                    </p>
+                <div className='w-full flex  gap-2 justify-start items-center shadow-md px-2 py-2 rounded-xl'>
+                    <LuGitCompare className='w-6 h-6 lg:w-8 lg:h-8 bg-blue-100 px-1 rounded-xl text-gray-600' />
+                    <div className='flex flex-col justify-start items-start'>
+                        <p className='text-sm lg:text-md font-semibold lg:font-semibold text-left'>
+                            Workforce Planning
+                        </p>
+                        <p className='text-left text-gray-500 font-semibold text-sm lg:text-md'>
+                            Scenarios + gaps
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div className='w-full flex  gap-2 justify-start items-center shadow-md px-2 py-2 rounded-xl'>
-                <IoMdTrendingUp className='w-6 h-6 lg:w-8 lg:h-8 bg-blue-100 px-1 rounded-xl text-gray-600' />
-                <div className='flex flex-col justify-start items-start'>
-                    <p className='text-sm lg:text-md font-semibold lg:font-semibold text-left'>
-                        Attrition & Risk
-                    </p>
-                    <p className='text-left text-gray-500 font-semibold text-sm lg:text-md'>
-                        Watchlists + drivers
-                    </p>
+                <div className='w-full flex  gap-2 justify-start items-center shadow-md px-2 py-2 rounded-xl'>
+                    <IoMdTrendingUp className='w-6 h-6 lg:w-8 lg:h-8 bg-blue-100 px-1 rounded-xl text-gray-600' />
+                    <div className='flex flex-col justify-start items-start'>
+                        <p className='text-sm lg:text-md font-semibold lg:font-semibold text-left'>
+                            Attrition & Risk
+                        </p>
+                        <p className='text-left text-gray-500 font-semibold text-sm lg:text-md'>
+                            Watchlists + drivers
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div className='w-full flex  gap-2 justify-start items-center shadow-md px-2 py-2 rounded-xl'>
-                <TbActivityHeartbeat className='w-6 h-6 lg:w-8 lg:h-8 bg-blue-100 px-1 rounded-xl text-gray-600' />
-                <div className='flex flex-col justify-start items-start'>
-                    <p className='text-sm lg:text-md font-semibold lg:font-semibold text-left'>
-                        Performance & Talent
-                    </p>
-                    <p className='text-left text-gray-500 font-semibold text-sm lg:font-md'>
-                        9-box + calibration
-                    </p>
+                <div className='w-full flex  gap-2 justify-start items-center shadow-md px-2 py-2 rounded-xl'>
+                    <TbActivityHeartbeat className='w-6 h-6 lg:w-8 lg:h-8 bg-blue-100 px-1 rounded-xl text-gray-600' />
+                    <div className='flex flex-col justify-start items-start'>
+                        <p className='text-sm lg:text-md font-semibold lg:font-semibold text-left'>
+                            Performance & Talent
+                        </p>
+                        <p className='text-left text-gray-500 font-semibold text-sm lg:font-md'>
+                            9-box + calibration
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div className='w-full flex  gap-2 justify-start items-center shadow-md px-2 py-2 rounded-xl'>
-                <CiDatabase className='w-6 h-6 lg:w-8 lg:h-8 bg-blue-100 px-1 rounded-xl text-gray-600' />
-                <div className='flex flex-col justify-start items-start'>
-                    <p className='text-sm lg:text-md font-semibold lg:font-semibold text-left'>
-                        Data Governace
-                    </p>
-                    <p className='text-left text-gray-500 font-semibold text-sm lg:text-md'>
-                        Integrity + audit
-                    </p>
+                <div className='w-full flex  gap-2 justify-start items-center shadow-md px-2 py-2 rounded-xl'>
+                    <CiDatabase className='w-6 h-6 lg:w-8 lg:h-8 bg-blue-100 px-1 rounded-xl text-gray-600' />
+                    <div className='flex flex-col justify-start items-start'>
+                        <p className='text-sm lg:text-md font-semibold lg:font-semibold text-left'>
+                            Data Governace
+                        </p>
+                        <p className='text-left text-gray-500 font-semibold text-sm lg:text-md'>
+                            Integrity + audit
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div className='w-full flex  gap-2 justify-start items-center shadow-md px-2 py-2 rounded-xl'>
-                <PiGitForkLight className='w-6 h-6 lg:w-8 lg:h-8 bg-blue-100 px-1 rounded-xl text-gray-600' />
-                <div className='flex flex-col justify-start items-start'>
-                    <p className='text-sm lg:text-md font-semibold lg:font-semibold text-left'>
-                        Global HR Ops
-                    </p>
-                    <p className='text-gray-600 text-left font-semibold text-sm lg:text-md'>
-                        Requests + SLA
-                    </p>
+                <div className='w-full flex  gap-2 justify-start items-center shadow-md px-2 py-2 rounded-xl'>
+                    <PiGitForkLight className='w-6 h-6 lg:w-8 lg:h-8 bg-blue-100 px-1 rounded-xl text-gray-600' />
+                    <div className='flex flex-col justify-start items-start'>
+                        <p className='text-sm lg:text-md font-semibold lg:font-semibold text-left'>
+                            Global HR Ops
+                        </p>
+                        <p className='text-gray-600 text-left font-semibold text-sm lg:text-md'>
+                            Requests + SLA
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div className='w-full flex  gap-2 justify-start items-center shadow-md px-2 py-2 rounded-xl'>
-                < MdOutlineEventNote className='w-6 h-6 lg:w-8 lg:h-8 bg-blue-100 px-1 rounded-xl text-gray-600' />
-                <div className='flex flex-col justify-start items-start'>
-                    <p className='text-sm lg:text-mdfont-semibold lg:font-semibold text-left'>
-                        Action Center
-                    </p>
-                    <p className='text-left text-gray-500 font-semibold text-sm lg:text-md'>
-                        Approvals + playbooks
-                    </p>
+                <div className='w-full flex  gap-2 justify-start items-center shadow-md px-2 py-2 rounded-xl'>
+                    < MdOutlineEventNote className='w-6 h-6 lg:w-8 lg:h-8 bg-blue-100 px-1 rounded-xl text-gray-600' />
+                    <div className='flex flex-col justify-start items-start'>
+                        <p className='text-sm lg:text-mdfont-semibold lg:font-semibold text-left'>
+                            Action Center
+                        </p>
+                        <p className='text-left text-gray-500 font-semibold text-sm lg:text-md'>
+                            Approvals + playbooks
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
